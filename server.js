5ed4a25c7cc1fd225b92900f558fc4421acd7b1e
Ben Redden <benjamminredden@gmail.com>
2019-12-02 12:16:48 -0500

    porting tesseract ocr from client to server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const Path = require('path');
 const Hapi = require('@hapi/hapi');
 const Inert = require('@hapi/inert');
+var Tesseract = require('tesseract.js');
+const fs = require('fs');
 
 const server = new Hapi.Server({
   port: 3333,
@@ -14,21 +16,65 @@ const server = new Hapi.Server({
   }
 });
 
+const doSomeOCR = function doSomeOCRFn(file) {
+  if (!file) {
+      file = __dirname + '/public/images/mmur/m7.png';
+  }
+
+  Tesseract.recognize(file)
+    .progress(function (p) { 
+console.log('progress', p) 
+    })
+    .catch(err => console.error(err))
+    .then(function (result) {
+console.log(result.text)
+      process.exit(0)
+    });
+};
+
 const provision = async () => {
 
   await server.register(Inert);
 
-  server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-      directory: {
-        path: '.',
-        redirectToSlash: true,
-        index: true,
-      }
+  server.route(
+    // {
+    //   method: 'GET',
+    //   path: '/home/{param*}',
+    //   handler: {
+    //     directory: {
+    //       path: '.',
+    //       redirectToSlash: true,
+    //       index: true,
+    //     }
+    //   }
+    // },
+    // {
+    //   method: 'GET',
+    //   path: '/view/{file*}',
+    //   handler: {
+    //     directory: {
+    //       path: 'upload'
+    //     }
+    //   }
+    // },
+    {
+      path: '/upload',
+      method: 'POST',
+      options: {
+        payload: {
+          output: 'stream',
+          parse: true
+        },
+        handler: async (req, h) => {
+console.log(req.payload);
+
+          doSomeOCR();
+
+          return h.response('req.payload');
+        }
+      }     
     }
-  });
+  );
 
   await server.start();
 
